30562f076dc06064c826f81acb5ec5f518a204f9
Oleg Lučić <183150217+oleglucic@users.noreply.github.com>
2026-08-16 00:35:43 +0200

    ci: simplify eslint config to fix HTML parsing error

--- eslint.config.js
+++ eslint.config.js
@@ -15,23 +15,5 @@ module.exports = [
       'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
       'no-console': 'off'
     }
-  },
-  {
-    files: ['*.html'],
-    languageOptions: {
-      parser: require('html-eslint/parser'),
-      ecmaVersion: 'latest',
-      sourceType: 'script',
-      globals: {
-        ...globals.browser,
-        ...globals.es2021
-      }
-    },
-    plugins: {
-      html: require('html-eslint/plugin')
-    },
-    rules: {
-      'no-console': 'off'
-    }
   }
 ];
